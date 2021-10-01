aacdb820d1f04d4ffb8ca05657048c7d09c533c2
Surinder75 <91563453+Surinder75@users.noreply.github.com>
2021-09-30 21:36:32 -0400

    Update DemoAgain.java

--- src/demo/DemoAgain.java
+++ src/demo/DemoAgain.java
@@ -3,6 +3,7 @@ package demo;
 public class DemoAgain {
 public static void main(String[] args) {
 	System.out.println("Again");
+	System.out.print("one more");
 	boolean isSunday =true;
 	if(isSunday) {
 		System.out.println("abc");
