82b4add18f5d6fa81c816d9e0f5ef9e8cd11d6b8
tanvirrandhawa <tanvirrandhawa@tanvirs-mbp.fios-router.home>
2021-09-30 21:13:01 -0400

    added condition

--- src/demo/DemoAgain.java
+++ src/demo/DemoAgain.java
@@ -3,5 +3,11 @@ package demo;
 public class DemoAgain {
 public static void main(String[] args) {
 	System.out.println("Again");
+	boolean isSunday =true;
+	if(isSunday) {
+		System.out.println("abc");
+	}else {
+		System.out.println("xyz");
+	}
 }
 }
